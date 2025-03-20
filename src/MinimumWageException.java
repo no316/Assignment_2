6f63063982bfac7cd3f755ab28fe86743bf6b299
Timothy Lee Kwan Weng <139492039+timlkwss@users.noreply.github.com>
2025-03-20 13:18:51 -0400

    Update MinimumWageException.java

--- src/MinimumWageException.java
+++ src/MinimumWageException.java
@@ -1,3 +1,8 @@
+//------------------------------------------------------------------------------------------------------------------
+//COMP249-Winter2025-Assg2
+//Written by: Timothy Lee Kwan Weng (40241060) & Noé Menacho (40306031)
+//Custom Exception Class for cases where hourly wage is less than minimum wage.
+//------------------------------------------------------------------------------------------------------------------
 public class MinimumWageException extends Exception{
     //Attribute
     public static final double MINIMUM_WAGE = 15.75;
