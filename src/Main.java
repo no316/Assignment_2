cb3aebc3b16bfa60c56143682d4d174e3711f9ad
Timothy Lee Kwan Weng <139492039+timlkwss@users.noreply.github.com>
2025-03-20 12:59:47 -0400

    welcome + closing messages added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args)  {
+        System.out.println("Hi welcome hooman! Greetings from Timothy Lee (40241060) and Noé Menacho (40306031)!!"); //welcome message
+        System.out.println("Now starting file cleaning & processing...");
         FileOperations.writeToFile(FileOperations.readFromFile("payroll.txt"),"payrollReport.txt");
+        System.out.println("Report successfully generated. Thank you for using iDroid Solution! Ciao!"); //closing message
     }
 }
